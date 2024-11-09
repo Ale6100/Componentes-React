00a53d5704af990df862819b46f764accd7be50b
Alejandro Portaluppi <alejandro_portaluppi@outlook.com>
2024-11-09 13:29:20 -0300

    Créditos agregados

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ export default function App() {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <a className='fixed bottom-[1vw] right-[1vw] hover:font-semibold text-sm max-md:text-xs hover:scale-105 hover:translate-x-[0.25vw] transition-all duration-100' href="https://www.linkedin.com/in/alejandro-portaluppi/" target="_blank" rel="noopener">Desarrollado por Alejandro P</a>
       <Toaster richColors theme='light' toastOptions={{}} position="top-right" closeButton/>
     </BrowserRouter>
   )
